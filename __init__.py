77a191fa7eaab8c7b28bed231134044bcf7f3814
capitan01R <hussainmoha95@gmail.com>
2026-01-14 05:24:26 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+from .capitan_zit_scheduler import (
+    NODE_CLASS_MAPPINGS,
+    NODE_DISPLAY_NAME_MAPPINGS
+)
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS"
+]
